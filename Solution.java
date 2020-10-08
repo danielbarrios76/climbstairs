e58dbc3afbc7f37acc1f285a750614cafebb6be7
Daniel Barrios <danielbarrios76@gmail.com>
2020-10-08 16:17:43 -0300

    add a test for solution

--- Solution.java
+++ Solution.java
@@ -24,6 +24,20 @@ class Solution {
     
     public static void main(String[] args) {
         // test the solution
+	    //test numbers
+	    int steps1 = 5;
+        int steps2 = 1;
+        int steps3 = 8;
+        int steps4 = 11;
+        int steps5 = 21;
+
+	    System.out.println("Ways to the top - Steps "+ steps1 + " : " + climbStairs(steps1));
+        System.out.println("Ways to the top - Steps "+ steps2 + " : " + climbStairs(steps2));
+        System.out.println("Ways to the top - Steps "+ steps3 + " : " + climbStairs(steps3));
+        System.out.println("Ways to the top - Steps "+ steps4 + " : " + climbStairs(steps4));
+        System.out.println("Ways to the top - Steps "+ steps5 + " : " + climbStairs(steps5));
+
+
 
 	}	
 }
